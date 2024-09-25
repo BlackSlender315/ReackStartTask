bacc828fb27dc999b275e7d17e4005a6a17457ab
BlackSlender315 <b.s.n.m.315@gmail.com>
2024-09-25 20:55:47 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
+function Welcome(props){
+  return <h1>{props.name}</h1>
+}
+function Greeting(prop){
+return <h1>Greetings Comander,{prop.name}</h1>
+}
+function TodoItem(props){
+  return <p>Задача:{props.task}</p>
+}
 function App() {
+  const [counter, setCounter] = useState(0)
+  const [array, setArray] = useState(["123", "132", "321", "231"])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Greeting name="Nikita"/>
+      <button type="button" onClick={()=>{setCounter(counter+1)}}>Я кнопка</button>
+      <TodoItem task={array[0]}/>
+      <TodoItem task={array[1]}/>
+
+      <p>Counter: {counter}</p>
     </div>
   );
 }
-
 export default App;
